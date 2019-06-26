d04bf6177d0e26351bd06f43df777b0163c321fc
HaviLee <liwenqiang@czb365.com>
2019-06-26 21:36:44 +0800

    Initial Commit

---
+++ SwiftUIDemo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by 李旭 on 2019/6/26.
+//  Copyright © 2019 HaviLee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
